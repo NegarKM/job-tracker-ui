810ffa4bfdfb4decae39028956b197392b4302ed
Negar Kermanshahimonfared <negar@Negars-MacBook-Pro.local>
2024-06-27 13:01:40 -0400

    job tracking page - basic search and tracking - static

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,179 @@
-import Image from "next/image";
+"use client";
 
-export default function Home() {
+import { useState } from 'react';
+
+const TRACKING_DATA = {
+  "BOOKMARKED": 2,
+  "APPLYING": 0,
+  "APPLIED": 1,
+  "INTERVIEWING": 0,
+  "REJECTED": 0,
+}
+
+const JOBS = [
+  {
+      "id": 1,
+      "description": null,
+      "jobTitle": "Senior Software Engineer",
+      "url": "http://sample-url.com",
+      "companyName": "Sample",
+      "location": null,
+      "status": "APPLIED"
+  },
+  {
+      "id": 2,
+      "description": "some description",
+      "jobTitle": "Software Engineer - Python",
+      "url": "http://madeup.com/careers/1234",
+      "companyName": "Madeup Co.",
+      "location": "Toronto",
+      "status": "BOOKMARKED"
+  },
+  {
+      "id": 3,
+      "description": "new description",
+      "jobTitle": "Software Engineer - Backend",
+      "url": "http://thiscompany.com/careers/1234",
+      "companyName": "thiscompany",
+      "location": "Remote",
+      "status": "BOOKMARKED"
+  }
+]
+
+type Job = {
+  id: number;
+  description: string | null;
+  jobTitle?: string;
+  url: string | null;
+  companyName: string | null;
+  location: string | null;
+  status: string | null;  
+}
+
+function TrackingButton({value, filterStatus, onFilterStatusChange}: {value: string, filterStatus: string, onFilterStatusChange: Function}) {
+  const count = TRACKING_DATA[value as keyof typeof TRACKING_DATA] ?? 0;
+  const formattedCount = count == 0 ? '--' : count
+
+  const handleClick = () => {
+    if (filterStatus.toLowerCase() === value.toLowerCase()) {
+      onFilterStatusChange('');
+    } else {
+      onFilterStatusChange(value);
+    }
+  };
+
+  let buttonClassName = "border border-dashed border-black font-sm w-40 p-4 m-4 font-semibold"
+  if (count > 0) {
+    buttonClassName += " hover:bg-orange-200"
+  }
+  if (filterStatus.toLowerCase() === value.toLowerCase()) {
+    buttonClassName += " bg-orange-200"
+  }
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <button 
+    className={buttonClassName}
+    disabled={count===0} 
+    onClick={handleClick}>
+      <div>{formattedCount}</div>
+      <div>{value}</div>
+    </button>
+  )
+}
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+function TrackingBar({filterStatus, onFilterStatusChange}: {filterStatus: string, onFilterStatusChange: Function}) {
+  return (
+    <div>
+      <TrackingButton value="BOOKMARKED" filterStatus={filterStatus} onFilterStatusChange={onFilterStatusChange}/>
+      <TrackingButton value="APPLYING" filterStatus={filterStatus} onFilterStatusChange={onFilterStatusChange}/>
+      <TrackingButton value="APPLIED" filterStatus={filterStatus} onFilterStatusChange={onFilterStatusChange}/>
+      <TrackingButton value="INTERVIEWING" filterStatus={filterStatus} onFilterStatusChange={onFilterStatusChange}/>
+      <TrackingButton value="REJECTED" filterStatus={filterStatus} onFilterStatusChange={onFilterStatusChange}/>
+    </div>
+  )
+}
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+function SearchBar({filterText, onFilterTextChange}: {filterText: string, onFilterTextChange: Function}) {
+  return (
+    <div>
+      <form>
+        <input 
+        type="text" 
+        placeholder="Filter Jobs" 
+        value={filterText} 
+        onChange={(e) => onFilterTextChange(e.target.value)}
+        className="border border-inherit font-sm w-40 m-4"/>
+      </form>
+    </div>
+  )
+}
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+function JobRow({job}: {job: Job}) {
+  let formattedStatus = job.status
+  if (job.status) {
+    formattedStatus = job.status.charAt(0).toUpperCase() + job.status.slice(1).toLowerCase()
+  }
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+  return (
+    <tr key={job.id} className="border">
+      <td className="border w-60">{job.jobTitle}</td>
+      <td className="border w-52">{job.companyName}</td>
+      <td className="border w-52">{job.location}</td>
+      <td className="border w-52">{formattedStatus}</td>
+    </tr>
   );
+}
+
+function JobTable({jobs, filterText, filterStatus}: {jobs: Job[], filterText: string, filterStatus: string}) {
+  const rows: JSX.Element[] = [];
+  jobs.forEach((job) => {
+    if (
+      (job.jobTitle === null || (job.jobTitle && job.jobTitle.toLowerCase().indexOf(filterText.toLowerCase()) === -1)) &&
+      (job.companyName === null || (job.companyName && job.companyName.toLowerCase().indexOf(filterText.toLowerCase()) === -1)) &&
+      (job.location === null || (job.location && job.location.toLowerCase().indexOf(filterText.toLowerCase()) === -1))
+    ) {
+      return;
+    }
+
+    if (job.status && job.status.toLowerCase().indexOf(filterStatus.toLowerCase()) === -1) {
+      return
+    }
+
+    rows.push(
+      <JobRow job={job} key={job.id} />
+    );
+  });
+
+  return (  
+    <div>
+        <table className="m-4 border">
+          <thead>
+            <tr className="border">
+              <th className="border">Job Position</th>
+              <th className="border">Company</th>
+              <th className="border">Location</th>
+              <th className="border">Status</th>
+            </tr>
+          </thead>
+          <tbody>{rows}</tbody>
+      </table>
+    </div>
+  )
+}
+
+function JobTrackerTable({jobs}: {jobs: Job[]}) {
+  const [filterText, setFilterText] = useState('');
+  const [filterStatus, setFilterStatus] = useState('');
+
+  return (
+    <div>
+      <TrackingBar filterStatus={filterStatus} onFilterStatusChange={setFilterStatus}/>
+      <SearchBar filterText={filterText} onFilterTextChange={setFilterText} />
+      <JobTable jobs={jobs} filterText={filterText} filterStatus={filterStatus} />
+    </div>
+  );
+}
+
+export default function App() {
+  return <JobTrackerTable jobs={JOBS} />;
 }
